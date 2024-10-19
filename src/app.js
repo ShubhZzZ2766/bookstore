29a955bea1b14ad132b985cbcdcc3ef00b7d86da
Shubham Cholke <shubham.cholke@intelligenttag.com>
2024-10-19 15:58:02 +0530

    Set up basic Express app and database connection

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const db = require('./config/db');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Bookstore API');
+});
+
+// Listen on port
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
